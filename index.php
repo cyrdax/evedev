6715b3db76c7280be0b283a83ba6af16e6d5d2bd
unknown <Reid@.(none)>
2014-06-27 03:40:01 -0700

    test commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+$typeids=array(34,35);
+$url="http://api.eve-central.com/api/marketstat?regionlimit=10000002&typeid=".join('&typeid=',$typeids);
+$pricexml=file_get_contents($url);
+$xml=new SimpleXMLElement($pricexml);
+foreach($typeids as $typeid)
+{
+    $item=$xml->xpath('/evec_api/marketstat/type[@id='.$typeid.']');
+    $price= (float) $item[0]->sell->percentile;
+    $price=round($price,2);
+    echo $typeid." ".$price."\n";
+}
+?>
